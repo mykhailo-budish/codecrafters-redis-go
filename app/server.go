a74c8f68698db78252001d9c3e8a6ffd2ef09e85
budish.mikhail <budish.mikhail@steelkiwi.com>
2024-03-22 18:19:58 +0200

    Add code for handling ping pong requests

--- app/server.go
+++ app/server.go
@@ -10,14 +10,34 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1<<12)
+	bytesRead, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		panic(err)
+	}
+
+	request := string(buffer[:bytesRead])
+	if request == "*1\r\n$4\r\nping\r\n" {
+		payload := []byte("+PONG\r\n")
+		conn.Write(payload)
 	}
 }
